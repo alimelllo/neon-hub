a2b647edfd30a308d912fd3857cc42a5f7554332
alimelllo <ali.melllo@yahoo.com>
2023-12-21 11:42:21 +0330

    header trace on server

--- src/Components/GeneralComponents/Header.tsx
+++ src/Components/GeneralComponents/Header.tsx
@@ -7,27 +7,27 @@ const Header = () => {
    
 
     return (
-        <header className='w-full h-[5rem] md:py-2 z-30 bg-[black] flex flex-row items-center'>
+        <header className='w-full h-[5rem] md:py-2 z-30 bg-[#56e1f0] flex flex-row items-center'>
             <div className='w-full h-[80%] flex z-40 flex-row justify-between'>
 
-                <div className='w-4/12 flex'>
+                <div className='w-4/12 flex bg-[blue]'>
                     {/* <div className='flex items-center w-5/12 md:w-full'>
                         <Image alt={'Neon Hub'} src={logo} />
                     </div> */}
                 </div>
 
 
-                <div id='navbar' className='w-4/12 text-[gray] font-[Vazir] text-[1.25rem] flex flex-row md:hidden items-center justify-center '>
-                    <button className='h-[70%] w-3/12 hover:text-[#ffffff] transition-all duration-200'>دسته</button>
+                <div className='w-4/12 text-[gray] bg-[pink] font-[Vazir] text-[1.25rem] flex flex-row md:hidden items-center justify-center '>
+                    {/* <button className='h-[70%] w-3/12 hover:text-[#ffffff] transition-all duration-200'>دسته</button>
                     <button className='h-[70%] w-3/12 hover:text-[#ffffff] transition-all duration-200'>محصولات</button>
-                    <button className='h-[70%] w-3/12 hover:text-[#ffffff] transition-all duration-200'>درباره ما</button>
+                    <button className='h-[70%] w-3/12 hover:text-[#ffffff] transition-all duration-200'>درباره ما</button> */}
                 </div>
 
 
-                <div className='w-4/12 text-[white] font-[Vazir] text-[1.25rem] md:text-[1rem] justify-end flex flex-row items-center pr-5 '>
-                    <div className='h-[80%] flex items-center w-4/12 md:w-full rounded-[5px] hover:text-[#ffffff] bg-gradient-to-r from-[#351578] to-[#e0546c]'>
+                <div className='w-4/12 bg-[green] text-[white] font-[Vazir] text-[1.25rem] md:text-[1rem] justify-end flex flex-row items-center pr-5 '>
+                    {/* <div className='h-[80%] flex items-center w-4/12 md:w-full rounded-[5px] hover:text-[#ffffff] bg-gradient-to-r from-[#351578] to-[#e0546c]'>
                         <button className='text-center bg-[black] hover:bg-transparent transition-all duration-200 w-[95%] h-[90%] mx-auto text-[#f1c2ca] hover:text-white'>ثبت نام / ورود</button>
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
